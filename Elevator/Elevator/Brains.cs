7c883dea7e8e138e76bcf4843cecce1a840a073b
Rebecca Scott <me@becdetat.com>
2018-12-11 09:49:49 +1000

    Set up basics

---
+++ Elevator/Elevator/Brains.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Elevator
+{
+    public class Brains
+    {
+        private readonly Queue<string> _commands = new Queue<string>();
+
+        public string CurrentFloor { get; private set; }
+
+        public void TakeCommand(string command)
+        {
+            _commands.Enqueue(command);
+        }
+
+        public void MoveToNextFloor()
+        {
+        }
+    }
+}
---
+++ Elevator/ElevatorTests/ItShouldHaveTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Shouldly;
+
+namespace ElevatorTests
+{
+    public class ItShouldHaveTests
+    {
+        [Test]
+        public void BecauseTestsAreGreat()
+        {
+            1.ShouldBe(1);
+        }
+    }
+}
